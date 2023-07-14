7de7020cb48b339c4b3b23c8c99e281e2ed5f6b2
Grace-ngigi <gracengigi859@gmail.com>
2023-07-14 11:50:01 -0700

    base class initialization

---
+++ models/base_model.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """
+    Base class defining all common attributes and methods for the other classes
+    """
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        sel.updated_at = self.created_at
+
+    def __str__(self):
+        """prints what base class is and what it does"""
+
+    def save(self):
+        """public instance method that updates updated_at"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionary containing all key/values"""
+        base_model_dict = self.__dict__.copy()
+        """returns only the instance attributes"""
+        base_model_dict['__class__'] = self.__class__._name__
+        base_nodel_dict['created_at'] = self.created_at.isoformat()
+        nase_model_dict['updated_at'] = self.updated_at.isoformat()
+        return base_model_dict
